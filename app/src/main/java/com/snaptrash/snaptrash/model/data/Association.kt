ebb6929221e9e98bcb0f5198f1d8e510a598a824
hany <hanyghorbel@gmail.com>
2023-03-29 19:13:34 +0300

    update to the association date class

--- app/src/main/java/com/snaptrash/snaptrash/model/data/Association.kt
+++ app/src/main/java/com/snaptrash/snaptrash/model/data/Association.kt
@@ -1,3 +1,14 @@
 package com.snaptrash.snaptrash.model.data
 
-data class Association()
+import java.util.*
+
+data class Association(
+    var name : String,
+    var phone_number : String,
+    var email : String,
+    var type : String,
+    var country : String,
+    var city : String,
+    var zip_code : String,
+    var street_number : String
+)
