38082b8c61e2943a462233ed1a45f9a0670898fd
hany <hanyghorbel@gmail.com>
2023-04-07 19:33:44 +0300

    Working on the Login ViewModel

--- app/src/main/java/com/snaptrash/snaptrash/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/snaptrash/snaptrash/viewmodel/LoginViewModel.kt
@@ -1,6 +1,36 @@
 package com.snaptrash.snaptrash.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.tasks.Task
+import com.google.firebase.functions.FirebaseFunctions
+
+
 
 class LoginViewModel: ViewModel() {
+    var email: String = ""
+    var password: String = ""
+    var isLogin: Boolean = false
+    //Initialize an instance of Cloud Functions
+    private val functions = FirebaseFunctions.getInstance()
+    private fun onLogin(email: String, password: String, isLogin: Boolean): Task<String> {
+        // Create the arguments to the callable function.
+        val data = hashMapOf(
+            "email" to email,
+            "password" to password,
+            "isLogin" to isLogin
+        )
+
+        return functions
+            .getHttpsCallable("onLogin")
+            .call(data)
+            .continueWith { task ->
+                // This continuation runs on either success or failure, but if the task
+                // has failed then result will throw an Exception which will be
+                // propagated down.
+                val result = task.result?.data as String
+                result
+            }
+    }
+
+
 }
