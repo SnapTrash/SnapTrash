26959d4a3f40f287db6fec9b93e91930a725d82a
GioGue <giorgiogueli2001@gmail.com>
2023-04-20 07:49:38 +0200

    HomeScreen withot map in the box

---
+++ app/src/main/java/com/snaptrash/snaptrash/view/HomeScreen/HomeScreen.kt
@@ -0,0 +1,62 @@
+package com.snaptrash.snaptrash.view.HomeScreen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun HomeScreen() {
+
+    Column(
+        modifier = Modifier.padding(40.dp)
+
+    ) {
+        Spacer(modifier = Modifier.height(50.dp))
+        Box(
+            Modifier
+                .height(400.dp)
+                .width(300.dp)
+
+                //.aspectRatio(1.0f) // fixed aspect ratio could be usefull to resize the map
+                .border(
+                    width = 2.dp,
+                    color = MaterialTheme.colorScheme.primary,
+
+                    )
+                .background(color = Color.White)
+        ) {
+            //put the initial map inside the box as in the screen in figma
+
+        }
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(
+            onClick = {  //on click go to CameraView already implement
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(70.dp)
+
+        ) { //button composable contains an other composable
+            Text(
+                //text = "Submit",
+                text = "Take a snap",
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+    }
+
+
+}
