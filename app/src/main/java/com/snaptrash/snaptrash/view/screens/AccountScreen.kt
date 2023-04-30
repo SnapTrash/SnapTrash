3e5cb744b1a7089b9cfeb47efd454504a9b87c82
GioGue <giorgiogueli2001@gmail.com>
2023-04-30 17:48:46 +0200

    ListItem for the Account screen unfinished.

--- app/src/main/java/com/snaptrash/snaptrash/view/screens/AccountScreen.kt
+++ app/src/main/java/com/snaptrash/snaptrash/view/screens/AccountScreen.kt
@@ -1,23 +1,33 @@
 package com.snaptrash.snaptrash.view.screens
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -34,8 +44,9 @@ import com.google.firebase.ktx.Firebase
 fun AccountScreen(){
     val checkStateSwitch = remember { mutableStateOf(false) }
     Column(
-        //horizontalAlignment = Alignment.CenterHorizontally,
-    ){
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Box(
             Modifier
                 .height(160.dp)
@@ -50,7 +61,6 @@ fun AccountScreen(){
                     color = MaterialTheme.colorScheme.primary,
                     shape = CircleShape
                 )
-                .align(Alignment.CenterHorizontally)
 
         ){
             Image(
@@ -76,6 +86,8 @@ fun AccountScreen(){
             modifier = Modifier.align(Alignment.CenterHorizontally))
         Spacer(modifier = Modifier.height(80.dp))
 
+
+/*
         Row(modifier = Modifier
             .padding(horizontal = 40.dp)
             .fillMaxWidth(),
@@ -98,6 +110,59 @@ fun AccountScreen(){
 
             )
         }
+
+
+        Row(modifier = Modifier
+            .padding(horizontal = 40.dp)
+            .fillMaxWidth(),
+            //horizontalArrangement= Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(text = "Password setting",
+                fontWeight = FontWeight.Normal, fontSize = 26.sp,
+                textAlign = TextAlign.Left
+            )
+            Spacer(modifier = Modifier.width(150.dp))
+            Icon(
+                Icons.Filled.KeyboardArrowRight,
+                contentDescription = "ArrowRight",
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier
+                    .size(32.dp)
+                    .align(Alignment.Bottom)
+                    .clickable(onClick = {})
+
+            )
+        }
+        Row(modifier = Modifier
+            .padding(horizontal = 40.dp)
+            .fillMaxWidth(),
+            //horizontalArrangement= Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(text = "Personal details",
+                fontWeight = FontWeight.Normal, fontSize = 26.sp,
+                textAlign = TextAlign.Left
+            )
+            Spacer(modifier = Modifier.width(150.dp))
+            Icon(
+                Icons.Filled.KeyboardArrowRight,
+                contentDescription = "ArrowRight",
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier
+                    .size(32.dp)
+                    .align(Alignment.Bottom)
+                    .clickable(onClick = {})
+
+            )
+
+        }
+
+ */
+
+        ListOptions()
+
+
         Spacer(modifier = Modifier.height(20.dp))
 
     }
@@ -106,3 +171,83 @@ fun AccountScreen(){
 
 
 }
+
+
+@Composable
+fun ListOptions() {
+    val configuration = LocalConfiguration.current
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ListItem(
+            modifier = Modifier
+                .width((configuration.screenWidthDp * 0.85).dp)
+                .clip(RoundedCornerShape(16.dp))
+                .align(Alignment.CenterHorizontally)
+                //.background(MaterialTheme.colorScheme.onPrimaryContainer, RoundedCornerShape(30.dp)),
+                .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(16.dp))
+                .clickable(onClick = {}), //go to screen to set this information
+            headlineContent = { Text("Personal details") },
+            leadingContent = {
+                Icon(
+                    Icons.Filled.Person,
+                    contentDescription = "Localized description",
+                )
+            }
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        ListItem(
+            modifier = Modifier
+                .width((configuration.screenWidthDp * 0.85).dp)
+                .clip(RoundedCornerShape(16.dp))
+                .align(Alignment.CenterHorizontally)
+                //.background(MaterialTheme.colorScheme.onPrimaryContainer, RoundedCornerShape(30.dp)),
+                .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(16.dp))
+                .clickable(onClick = {}), //go to screen to set this information
+            headlineContent = { Text("Password settings") },
+            leadingContent = {
+                Icon(
+                    Icons.Filled.Password,
+                    contentDescription = "Localized description",
+                )
+            }
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+        ListItem(
+            modifier = Modifier
+                .width((configuration.screenWidthDp * 0.85).dp)
+                .clip(RoundedCornerShape(16.dp))
+                .align(Alignment.CenterHorizontally)
+                //.background(MaterialTheme.colorScheme.onPrimaryContainer, RoundedCornerShape(30.dp)),
+                .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(16.dp))
+                .clickable(onClick = {}), //go to screen to set this information
+            headlineContent = { Text("Notification") },
+            leadingContent = {
+                Icon(
+                    Icons.Filled.Notifications,
+                    contentDescription = "Localized description",
+                )
+            }
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+        ListItem(
+            modifier = Modifier
+                .width((configuration.screenWidthDp * 0.85).dp)
+                .clip(RoundedCornerShape(16.dp))
+                .align(Alignment.CenterHorizontally)
+                //.background(MaterialTheme.colorScheme.onPrimaryContainer, RoundedCornerShape(30.dp)),
+                .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(16.dp))
+                .clickable(onClick = {}), //go to screen to set this information
+            headlineContent = { Text("Payment") },
+            leadingContent = {
+                Icon(
+                    Icons.Filled.CreditCard,
+                    contentDescription = "Localized description",
+                )
+            }
+        )
+    }
+}
+
